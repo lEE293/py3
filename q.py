b57412cedef324719bd4b6137d582a00ad08d34c
lEE293 <115855759+lEE293@users.noreply.github.com>
2024-03-15 12:35:38 +0900

    Add files via upload

---
+++ q.py
@@ -0,0 +1,11 @@
+Python 3.11.7 (tags/v3.11.7:fa7a6f2, Dec  4 2023, 19:24:49) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> q 5 //3
+SyntaxError: invalid syntax
+>>> q = 5 // 3
+>>> r = 5 % 3
+>>> print(q, r)
+1 2
+>>> num = 5 ** 3
+>>> print(q, r, num)
+1 2 125
