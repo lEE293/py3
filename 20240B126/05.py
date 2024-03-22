aaefb16ff9562f859fe47075de449e61cb27ee77
lEE293 <115855759+lEE293@users.noreply.github.com>
2024-03-22 12:34:44 +0900

    Delete 20240B126 directory

--- 20240B126/05.py
+++
@@ -1,2 +0,0 @@
-score = int(input("필시 점수를 입력하세요 : "))
-print(score >= 70)
--- 20240B126/lab03.py
+++
@@ -1,7 +0,0 @@
-#int 사용시 소수점 안 나옴
-#float 사용시 소수점까지 나옴
-
-pound = float(input("파운드(1b)를 입력하세요 : "))
-kg = pound * 0.453592
-#print(pound, "파운드(1b)는", kg, "킬로그램(kg)입니다")
-print("%d 파운드는 %.2f 킬로그램입니다" %(pound,kg))
